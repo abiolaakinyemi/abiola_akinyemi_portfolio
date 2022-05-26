6710a14a44ce80e23c43d5e3b341256476d3aa8d
abiolaakinyemi <84160458+abiolaakinyemi@users.noreply.github.com>
2022-05-26 07:06:42 -0400

    main

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,6 @@
 import email from "../assets/email.png";
 import mobile from "../assets/mobile.png";
 import api from "../assets/api.png";
-import cpp from "../assets/cpp.png";
 import css from "../assets/css.png";
 import figma from "../assets/figma.png";
 import flutter from "../assets/flutter.png";
@@ -11,35 +10,28 @@ import html from "../assets/html.png";
 import javascript from "../assets/javascript.png";
 import mu5 from "../assets/mu5.png";
 import node from "../assets/node.png";
-import python from "../assets/python.png";
 import react from "../assets/react.png";
 import redux from "../assets/redux.png";
 import sass from "../assets/sass.png";
 import typescript from "../assets/typescript.png";
 import vue from "../assets/vue.png";
-
+import logo from "../assets/logo.png";
 import about01 from "../assets/about01.png";
 import about02 from "../assets/about02.png";
 import about03 from "../assets/about03.png";
 import about04 from "../assets/about04.png";
-
 import profile from "../assets/profile.png";
 import circle from "../assets/circle.svg";
-import logo from "../assets/logo.png";
-
 import adidas from "../assets/adidas.png";
 import amazon from "../assets/amazon.png";
-import asus from "../assets/asus.png";
 import bolt from "../assets/bolt.png";
-import nb from "../assets/nb.png";
-import skype from "../assets/skype.png";
 import spotify from "../assets/spotify.png";
 
 export default {
   email,
+  logo,
   mobile,
   api,
-  cpp,
   css,
   figma,
   flutter,
@@ -49,7 +41,6 @@ export default {
   javascript,
   mu5,
   node,
-  python,
   react,
   redux,
   sass,
@@ -61,12 +52,8 @@ export default {
   about04,
   profile,
   circle,
-  logo,
   adidas,
   amazon,
-  asus,
   bolt,
-  nb,
-  skype,
   spotify,
 };
